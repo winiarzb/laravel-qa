7ef9b58cecb2a41718fe007ca26cfa94952f48b1
winiarzb <l3arteq@gmail.com>
2020-03-03 08:38:24 +0100

    added main view

---
+++ app/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    protected $fillable = ['title','body'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+    public function getUrlAttribute() {
+        return route('question.show', $this->id);
+    }
+    public function getCreatedDateAttribute() {
+        return $this->created_at->diffForHumans();
+    }
+}
